babb393cc4fc3da352f3fa9fadf35006a1be547a
Hekar <hekark@gmail.com>
2013-02-26 14:46:37 -0500

    added stuff

--- root/index.php
+++ root/index.php
@@ -1,12 +1,18 @@
 <html>
   <head>
     <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/button.css" />
+    <link rel="stylesheet" type="text/css" href="css/ct.css" />
+    
     <script type="text/javascript" src="js/main.js">
     </script>
   </head>
   
   <body>
+    <noscript><h1 style="margin:50px">Please enable Javascript</h1></noscript>
+  
     <div class="header">
+      <a class="back"     href="#"><div>Back</div></a>
     </div>
     
     <div class="content">
@@ -14,6 +20,9 @@
     </div>
     
     <div class="footer">
+      <a class="home"     href="#"><img src="img/home.png" /><div>Home</div></a>
+      <a class="ct-logo"  href="#"><div></div></a>
+      <a class="help"     href="#"><div>Help</div></a>
     </div>
   </body>
 
