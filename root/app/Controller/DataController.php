ae7ab461b503d37635f28a646d916cf6e49e94e1
Tom-Bob <kempton.tom@gmail.com>
2013-03-04 09:45:00 -0500

    added data controller

--- root/app/Controller/DataController.php
+++ root/app/Controller/DataController.php
@@ -29,54 +29,41 @@ App::uses('AppController', 'Controller');
  * @package       app.Controller
  * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
-class PagesController extends AppController {
+class DataController extends AppController {
 
-/**
- * Controller name
- *
- * @var string
- */
-	public $name = 'Pages';
-
-/**
- * This controller does not use a model
- *
- * @var array
- */
-	public $uses = array();
-
-/**
- * Displays a view
- *
- * @param mixed What page to display
- * @return void
- */
-	public function display() {
-		$path = func_get_args();
-
-		$count = count($path);
-		if (!$count) {
-			$this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
-
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-		$this->render(implode('/', $path));
-	}
-  
-    // json:
-    // http://gablog.eu/online/node/92.html
-
+    /**
+     * Controller name
+     *
+     * @var string
+     */
+	public $name = 'Data';
+    
+    /**
+     * Reads a variable from the request
+     */
+    private function getVar($var) {
+        if (isset($_REQUEST[$var])) {
+            return $_REQUEST[$var];
+        }
+        else {
+            return null;
+        }
+    }
+    
+    /*
+     * JSON FORMAT:
+     * array of:
+     *  {
+     *      text: "",
+     *      value: "",
+     *      checked: true/false
+     *  }
+     *
+     */
+     
+     
     public function jsonFunction() {
-        $this->respond('stuff');
+        $year = $this->getVar('year');
+        return new CakeResponse(array('body' => json_encode($year)));
     }
 }
